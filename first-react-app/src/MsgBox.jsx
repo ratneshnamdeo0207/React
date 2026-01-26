89ba781b57ce4c58518d95b71a6362661a10c554
ratneshnamdeo0207 <ratneshnamdeo0207@gmail.com>
2026-01-26 05:36:11 +0530

    addition more components

---
+++ first-react-app/src/MsgBox.jsx
@@ -0,0 +1,6 @@
+export default function MsgBox({name, c})
+{
+    
+    // let s = {color : c};
+    return <h2 style={{color : c}}>{name}</h2>;
+}
---
+++ first-react-app/src/Title.jsx
@@ -0,0 +1,5 @@
+function Title()
+{
+  return <h1>2 x 2 = {2*2}</h1>;
+}
+export default Title;
